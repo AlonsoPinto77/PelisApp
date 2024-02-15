a4985206e5cdcb7226a069c4db30a0ec1421ebe0
AlonsoPinto77 <42550944+AlonsoPinto77@users.noreply.github.com>
2024-02-15 12:49:55 -0500

    Animation exit to left added, pending unit tests.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":domain")))
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.android.support:multidex:1.0.3")
---
+++ app/src/test/java/com/example/pelisapp/ui/catalogue/CatalogueFragmentTest.kt
@@ -0,0 +1,19 @@
+package com.example.pelisapp.ui.catalogue
+
+import org.junit.jupiter.api.Assertions.*
+
+class CatalogueFragmentTest {
+
+    @org.junit.jupiter.api.Test
+    fun getTargetPage() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getPageSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setPageSize() {
+    }
+}
